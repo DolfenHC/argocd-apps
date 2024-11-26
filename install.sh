8931c4ae7d104f0c9d85c6c0216ba3643fa8e742
Guido Dolfen <44291904+gdolfen@users.noreply.github.com>
2024-11-26 19:16:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
-curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --disable traefik
-sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod +x ./get_helm.sh
+./get_helm.sh
+k3d cluster create --port 80:80@loadbalancer --k3s-arg="--disable=traefik@server:0"
 helm repo add argo-cd https://argoproj.github.io/argo-helm
 helm install argocd argo-cd/argo-cd --values argocd-values.yaml --namespace=argocd --create-namespace
 helm template charts/root-app/ | kubectl apply -f -
