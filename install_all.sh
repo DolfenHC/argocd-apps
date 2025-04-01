d5c54b5732f5bee3ac56e435c019e84af4ecd438
Guido Dolfen <guido.dolfen@haeger-consulting.de>
2025-04-01 14:18:52 +0200

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -3,5 +3,7 @@ sudo apt update
 sudo apt install git
 git clone https://github.com/DolfenHC/argocd-apps.git
 cd argocd-apps
+chmod +x ./install_docker.sh
 chmod +x ./install_k3d.sh
+./install_docker.sh
 ./install_k3d.sh
